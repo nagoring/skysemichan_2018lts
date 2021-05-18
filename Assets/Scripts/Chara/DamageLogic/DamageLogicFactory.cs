0ffe999de8f80e207af63d25f13b6b7fbd4afcb2
nagomi <nagomi.github@gmail.com>
2021-05-19 02:38:08 +0900

    CalcDamageの抽象化

---
+++ Assets/Scripts/Chara/DamageLogic/DamageLogicFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Skysemi.With.Chara.DamageLogic;
+using UnityEngine;
+
+namespace Skysemi.With.Chara.DamageLogic
+{
+    public class DamageLogicFactory
+    {
+        public static IDmageLogic create(IChara target, IChara iChara)
+        {
+            return new StandartLogic();
+        }
+    }    
+}
+
+
+
+    
---
+++ Assets/Scripts/Chara/DamageLogic/StandartLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang;
+using UnityEngine;
+
+namespace Skysemi.With.Chara.DamageLogic
+{
+    public class StandartLogic : IDmageLogic
+    {
+    
+        public int CalcDamage(IChara target, IChara self)
+        {
+        
+            int damage = self.Atk - target.Def;
+            damage += (int)Random.Range(-3.0f, 3.0f);
+            if (damage < 0) damage = 0;
+            return damage;
+        }
+    }
+}
